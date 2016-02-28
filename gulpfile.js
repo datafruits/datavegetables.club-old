e877a41076b9da0a1cec514a02b9f89a6623beae
Tony Miller <mcfiredrill@gmail.com>
2016-02-28 11:44:12 +0900

    add awspublish

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var reload = browserSync.reload;
 
 var through2 = require('through2');
 var browserify = require('browserify');
+var awspublish = require('gulp-awspublish');
 
 gulp.task('stylesheet', ['sprites'], function () {
   return gulp.src('app/css/main.scss')
@@ -190,4 +191,35 @@ gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras'], function () 
 
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
+});
+
+gulp.task('publish', function() {
+
+  // create a new publisher using S3 options
+  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
+  var publisher = awspublish.create({
+    params: {
+      Bucket: 'datavegetables.club'
+    }
+  });
+
+  // define custom headers
+  var headers = {
+    'Cache-Control': 'max-age=315360000, no-transform, public'
+    // ...
+  };
+
+  return gulp.src('./dist/**/*')
+     // gzip, Set Content-Encoding headers and add .gz extension
+    //.pipe(awspublish.gzip({ ext: '.gz' }))
+
+    // publisher will add Content-Length, Content-Type and headers specified above
+    // If not specified it will set x-amz-acl to public-read by default
+    .pipe(publisher.publish(headers))
+
+    // create a cache file to speed up consecutive uploads
+    //.pipe(publisher.cache())
+
+     // print upload updates to console
+    .pipe(awspublish.reporter());
 });
